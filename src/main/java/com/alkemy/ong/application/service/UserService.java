0372fde2306eb5e1a10458012b0c2a26ae1329ec
Ricardo Olivera <rholivera93@gmail.com>
2022-08-21 06:31:40 -0300

    create user service

---
+++ src/main/java/com/alkemy/ong/application/service/UserService.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.application.service;
+
+import com.alkemy.ong.application.service.usecase.ICreateUserUseCase;
+import com.alkemy.ong.domain.User;
+
+public class UserService implements ICreateUserUseCase {
+
+  @Override
+  public User addUser(User add) {
+    return null;
+  }
+}
