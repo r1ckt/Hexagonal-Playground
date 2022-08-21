0335a0c534e07e007aff4b5c2e68a5447a6eb16c
Ricardo Olivera <rholivera93@gmail.com>
2022-08-21 14:09:03 -0300

    create encoder, modify securityconfig

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
@@ -1,20 +1,46 @@
 package com.alkemy.ong.infrastructure.config.spring.security;
 
+import com.alkemy.ong.infrastructure.config.spring.security.filter.AuthorizationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @EnableWebSecurity
 @Configuration
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final AuthorizationFilter authorizationFilter;
+  private final UserDetailsService userDetailsService;
+  private final PasswordEncoder passwordEncoder;
+
+  @Override
+  public void configure(AuthenticationManagerBuilder managerBuilder) throws Exception {
+
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().anyRequest().permitAll();
   }
 
+  //http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+  /*
+  @Override
+  @Bean
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+*/
 }
--- src/main/java/com/alkemy/ong/infrastructure/rest/request/UserRegisterRequest.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/UserRegisterRequest.java
@@ -1,7 +1,7 @@
 package com.alkemy.ong.infrastructure.rest.request;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,11 +12,13 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UserRegisterRequest {
 
+  @NotNull(message = "Name cannot be null")
   private String firstName;
 
+  @NotNull(message = "Last name cannot be null")
   private String lastName;
 
-  @Email(message = "Enter a valid email")
+  @Email(message = "Email should be valid")
   private String email;
 
   @Size(min = 8, max = 16, message = "Password must be between 8 and 16 characters long")
